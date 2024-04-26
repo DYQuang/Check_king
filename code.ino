0ba0bfee83d81875a5ada17cbca46fe8cdb506aa
DYQuang <142239542+DYQuang@users.noreply.github.com>
2024-04-27 00:41:33 +0700

    Add files via upload

--- code.ino
+++ code.ino
@@ -1,146 +1,144 @@
-#define BLYNK_TEMPLATE_ID "TMPL2BE2K7LO2"
-#define BLYNK_TEMPLATE_NAME "vuon thong minh"
-#define BLYNK_AUTH_TOKEN "3JrlG_6jRYRd5uYDjMkViqU5mSgP9ApP"
-#define BLYNK_WIFI_SSID "123123123"
-#define BLYNK_WIFI_PASS "DuyQuang"
-#define BLYNK_PRINT Serial
-
+#include <Servo.h>
+#include <MFRC522.h>
 #include <ESP8266WiFi.h>
 #include <WiFiManager.h>
-#include <BlynkSimpleEsp8266.h>
-#include <DHTesp.h>
-#include <Wire.h>
 #include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SH110X.h>
 
-#define i2c_Address 0x3c
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET -1   //   QT-PY / XIAO
-#define DHTPIN D5    // Digital pin connected to the DHT sensor (temperature and humidity)
-#define DHTTYPE DHT11 // Type of DHT sensor
+#define RST_PIN D3
+#define SS_PIN D4
+#define GAS_SENSOR_PIN A0
+#define fan_pin D1
+#define fire_pin D0
+#define buzz_pin D2
 
-#define relay_bom D6
-#define relay_led D7
-#define light_PIN D0
-#define MOISTURE_PIN A0
-int mode = 0;  // nếu là 1: chế độ tự động, nếu là 0: chế độ thủ công
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+Servo SERVO1;
+Servo SERVO2;
 
-Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-WiFiManager wifimanager;
-DHTesp dht;
+bool isFirstSwipe = true;
+bool isDoorOpen = false;
 
-     
-void setup() 
+void setup()
 {
   Serial.begin(115200);
-  dht.setup(DHTPIN, DHTesp::DHT11);
-  pinMode(relay_led,OUTPUT);
-  pinMode(relay_bom,OUTPUT);
-  pinMode(light_PIN,INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(relay_led,LOW);
-  digitalWrite(relay_bom,LOW);
-  digitalWrite(LED_BUILTIN, LOW);
-  display.begin(i2c_Address, true);
-  display.display();
-  display.clearDisplay();
-  display.begin(i2c_Address, true);
-  display.display();
-  display.clearDisplay();
-  display.setTextColor(SH110X_WHITE);
-  display.setTextSize(2);
-  display.setCursor(10, 0);
-  display.println("Thong Tin");
-  display.setTextSize(1);
-  display.setCursor(0, 20);
-  display.println("Temp: ");
-  display.setCursor(0, 30);
-  display.println("Humi: ");
-  display.setCursor(0, 40);
-  display.println("Soil Moisture: ");
-  
-  display.setTextSize(1);
-  display.setCursor(60, 20);
-  display.println("*C");
-  display.setCursor(60, 30);
-  display.println("%");
-  display.setCursor(120, 40);
-  display.println("%");
-  wifimanager.autoConnect("config wifi");
-  Blynk.begin(BLYNK_AUTH_TOKEN, WiFi.SSID().c_str(), WiFi.psk().c_str());
-  digitalWrite(LED_BUILTIN, HIGH);
-  
+  SPI.begin();
+  mfrc522.PCD_Init();
+  SERVO1.attach(D8);
+  SERVO2.attach(3);
+  pinMode(fan_pin, OUTPUT);
+  pinMode(buzz_pin, OUTPUT);
+  pinMode(fire_pin, INPUT);
+  digitalWrite(buzz_pin, LOW);
+  digitalWrite(fan_pin, LOW);
+  SERVO1.write(0);
+  SERVO2.write(0);
+  Serial.println("Đưa thẻ RFID vào phạm vi đọc...");
+
+  WiFiManager wifiManager;
+  if (!wifiManager.autoConnect("Hệ Thống PCCC", "11111111")) 
+  {
+    Serial.println("Failed to connect and hit timeout");
+    delay(3000);
+    ESP.reset();
+    delay(5000);
+  }
+  Serial.println("Connected to WiFi!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  SPI.begin();
+  Serial.println("Kết nối WiFi thành công!");
+
+  // Hiển thị thông tin tên và mật khẩu WiFi
+  Serial.print("WiFi SSID: ");
+  Serial.println(WiFi.SSID());
+  Serial.print("WiFi Password: ");
+  Serial.println(WiFi.psk());
 }
 
-void loop() 
+void loop()
 {
-  Blynk.run();
-  float humidity = dht.getHumidity();
-  float temperature = dht.getTemperature();
-  int light_value = digitalRead(light_PIN);
-  int MOISTUREStatus = analogRead(MOISTURE_PIN);
-  int moisturePercentage = map(MOISTUREStatus, 0, 1023, 0, 100);
+  rfid_check();
+  MQ5();
+  check_fire();
+  delay(500);
+}
 
-
-  if(light_value == LOW)
-  {
-    Serial.println("DayTIME");
-    digitalWrite(relay_led,HIGH);
-  }
-  else
-  {
-    Serial.println("NIGHT");
-    digitalWrite(relay_led,LOW);
+void rfid_check()
+{
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
   }
 
-  if(mode == 1) 
-  { // Chế độ auto
-    int MOISTUREStatus = analogRead(MOISTURE_PIN);
-    int moisturePercentage = map(MOISTUREStatus, 0, 1023, 0, 100);
-    if (moisturePercentage < 50) 
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  unsigned long uidDec = 0;
+  Serial.println("===================================");
+  Serial.println("Card UID: ");
+  
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    uidDec = (uidDec * 256) + mfrc522.uid.uidByte[i];
+  }
+  Serial.print(uidDec);
+  
+  if (uidDec == 2736406578)
+  {
+    if (isFirstSwipe) 
     {
-      digitalWrite(relay_bom, HIGH);
+      SERVO1.write(180);
+      isFirstSwipe = false;
+      isDoorOpen = true;
     } 
-    else
+    else 
     {
-      digitalWrite(relay_bom, LOW);
+      SERVO1.write(0);
+      isFirstSwipe = true;
+      isDoorOpen = false;
     }
   }
-  Serial.println(MOISTUREStatus);
 
-
-  delay(1000);
-  // Gửi nhiệt độ và độ ẩm lên ứng dụng Blynk
-  Blynk.virtualWrite(V2, humidity);
-  Blynk.virtualWrite(V3, temperature);
-  Blynk.virtualWrite(V4, moisturePercentage);
-  display.setCursor(30, 20);
-  display.println(temperature);
-  display.setCursor(30, 30);
-  display.println(humidity);
-  display.setCursor(100, 40);
-  display.println(moisturePercentage);
-  display.display();
+  mfrc522.PICC_HaltA();
 }
 
-BLYNK_WRITE(V0) 
+void MQ5()
 {
-  int check_auto = param.asInt();
-  if(check_auto == 1)
+  int gasValue = analogRead(GAS_SENSOR_PIN);
+  int mappedValue = map(gasValue, 0, 1023, 0, 225);
+  
+  Serial.print("Gas Sensor Value: ");
+  Serial.println(mappedValue);
+
+  if (mappedValue > 100)
   {
-    mode = 1;
+    digitalWrite(fan_pin, HIGH);
   }
   else
   {
-    mode = 0;
+    digitalWrite(fan_pin, LOW);
   }
+  
+  delay(1000);
 }
 
-BLYNK_WRITE(V1)
+void check_fire()
 {
-  int value = param.asInt();
-  digitalWrite(relay_bom,value);
+  int fireValue = digitalRead(fire_pin);
+  
+  if (fireValue == LOW)
+  {
+    Serial.println("Phát hiện lửa!");
+    digitalWrite(buzz_pin, HIGH);
+    SERVO1.write(180);
+    SERVO2.write(180);
+  }
+  else 
+  { 
+    Serial.println("Bình thường");
+    digitalWrite(buzz_pin, LOW);
+    SERVO1.write(0);
+    SERVO2.write(0);
+  }
+  
+  delay(1000);
 }
